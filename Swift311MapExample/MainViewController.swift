ba392e7296598dce4712b7346be42fe33c3a2798
Joshua Kaden <jkaden@doitt.nyc.gov>
2017-12-14 17:00:22 -0500

    create ServiceRequest struct & network client

--- Swift311MapExample/MainViewController.swift
+++ Swift311MapExample/MainViewController.swift
@@ -9,5 +9,16 @@
 import UIKit
 
 class MainViewController: UITabBarController {
+    private let client = ServiceRequestClient()
+    private let defaultLatitude: Double = 40.759211
+    private let defaultLongitude: Double = -73.984638
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        client.getServiceRequests(latitude: defaultLatitude, longitude: defaultLongitude, withinCircle: 350) {
+            serviceRequests in
+            serviceRequests.map { print($0) }
+        }
+    }
 }
