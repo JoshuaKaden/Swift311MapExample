a30d12551d9c275a1dddca1a377e3573e37ec978
Joshua Kaden <jkaden@doitt.nyc.gov>
2017-12-14 14:11:11 -0500

    center map on user location

--- Swift311MapExample/FirstViewController.swift
+++ Swift311MapExample/FirstViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet private weak var mapView: MKMapView!
+    
+    private let locationManager = CLLocationManager()
+    fileprivate var userLocation: CLLocation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,31 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        locationManager.startUpdatingLocation()
+    }
+    
+    fileprivate func centerMap() {
+        guard let userLocation = userLocation else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 
 }
 
+extension FirstViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+        locationManager.stopUpdatingLocation()
+        
+        if let userLocation = userLocation {
+            if userLocation.coordinate.latitude == location.coordinate.latitude && userLocation.coordinate.longitude == location.coordinate.longitude {
+                return
+            }
+        }
+        
+        userLocation = location
+        centerMap()
+    }
+}
